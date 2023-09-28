ce222e22f4fffbe89e2860ef7888717d4a9f0193
bartdebeus <145577876+bartdebeus@users.noreply.github.com>
2023-09-28 13:57:40 +0200

    Update case2_streamlit_app.py

--- case2_streamlit_app.py
+++ case2_streamlit_app.py
@@ -11,8 +11,6 @@ path = 'C:/Users/bartd/OneDrive/Bureaublad/Data Science (Minor)/'
 import pandas as pd
 import streamlit as st
 
-import matplotlib.pyplot as plt
-import seaborn as sns
 import plotly.express as px
 
 from PIL import Image
